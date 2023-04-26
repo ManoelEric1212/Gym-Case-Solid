ec531ba2806a5fa8ab01ebf90183300d67873493
ManoelEric1212 <manoeleric59@gmail.com>
2023-04-26 00:51:34 -0300

    feat(Users): Implementando repository pattern e inversão de dependência

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply} from 'fastify'
 import { z } from 'zod'
-import { registerUseCase } from '../../use-cases/register'
+import { RegisterUseCase } from '../../use-cases/register'
+import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository'
 export async function register(request: FastifyRequest, reply: FastifyReply){
   const registerBodySchema = z.object({
     name: z.string(),
@@ -10,10 +11,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply){
   const {name, email, password} = registerBodySchema.parse(request.body)
 
   try {
-    await registerUseCase({name,email,password})
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+    await registerUseCase.execute({name,email,password})
   } catch (err) {
     return reply.status(409).send()
-    
   }
 
   return reply.status(201).send()
---
+++ src/repositories/users-repository.ts
@@ -0,0 +1,7 @@
+import { User, Prisma } from "@prisma/client";
+
+// Atentar para o uso de uma DTO caso não estiver usando prisma
+export interface UsersRepository {
+  findByEmail(email:string): Promise<User | null>
+  create(data: Prisma.UserCreateInput): Promise<User>
+}
