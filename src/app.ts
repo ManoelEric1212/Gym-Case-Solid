a71883ff7c701f260d2cfdfcaeeeb6cc0fb4fa2b
ManoelEric1212 <manoeleric59@gmail.com>
2023-04-26 08:09:55 -0300

    refactor(Users): Inserindo tratativa global para erros, e formatando mensagem do OZd

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 export const app = fastify();
 
 app.register(appRoutes)
-// const prisma = new PrismaClient();
+
+app.setErrorHandler((error,_,reply)=>{
+  if(error instanceof ZodError){
+    return reply.status(400).send({message: 'Validation error.', issues: error.format()})
+  }
+  if(env.NODE_ENV !== "production"){
+    console.error(error)
+  } else {
+    // Fazer um log para uma ferramenta externa como DataLog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({message: 'Internal server error!!'})
+
+})
